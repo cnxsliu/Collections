aa631fe9a22a24622cc8d8faa9171cd49bd874a1
cnxsliu <1283216796@qq.com>
2020-05-18 10:05:54 +0800

    change names

---
+++ src/main/java/cnxsliu/exams/Main01.java
@@ -0,0 +1,10 @@
+package cnxsliu.cnxsliu.exams;
+
+/**
+ * @author cnxsliu
+ * @description
+ * @date 2020/5/18 10:03
+ * @strategy
+ */
+public class Main01 {
+}
---
+++ src/main/java/cnxsliu/exams/Main02.java
@@ -0,0 +1,10 @@
+package cnxsliu.exams;
+
+/**
+ * @author cnxsliu
+ * @description
+ * @date 2020/5/18 10:05
+ * @strategy
+ */
+public class Main02 {
+}
